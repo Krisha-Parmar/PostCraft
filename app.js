e0920b460f4648a8939cf8a072d1a1367524010c
Krisha Parmar <krishaparmar710@gmail.com>
2024-07-26 21:36:06 +0530

    add like edit features

--- app.js
+++ app.js
@@ -18,9 +18,46 @@ app.get("/login",(req,res)=>{
     
     res.render('login');
 })
-app.get("/profile",isLoggedIn,(req,res)=>{
-    console.log(req.user);
-    res.render('login');
+app.get("/profile",isLoggedIn,async (req,res)=>{//protected route
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+    // console.log(user)
+    //because we have post in id form
+    res.render('profile',{user});
+})
+app.get("/like/:id",isLoggedIn,async (req,res)=>{//protected route
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    if(post.likes.indexOf(req.user.userid)=== -1){
+        post.likes.push(req.user.userid);
+
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+    }
+    await post.save();
+    res.redirect('/profile');
+})
+
+app.get("/edit/:id",isLoggedIn,async (req,res)=>{//protected route
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    res.render('edit',{post});
+})
+app.post("/update/:id",isLoggedIn,async (req,res)=>{//protected route
+    let post = await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content});
+    
+    res.redirect('/profile');
+})
+
+app.post("/post",isLoggedIn,async (req,res)=>{//protected route
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+
+    });
+    user.posts.push(post._id); 
+    await user.save();
+    res.redirect("/profile")
 })
 app.post("/register", async(req,res)=>{
 
@@ -41,7 +78,7 @@ app.post("/register", async(req,res)=>{
 
             let token = jwt.sign({email: email,userid: user._id},"shhhh");
             res.cookie("token",token);
-            res.send("registered");
+            res.redirect("/login");
         })
 
 
@@ -59,7 +96,7 @@ app.post("/login", async(req,res)=>{
         if(result){ 
             let token = jwt.sign({email: email,userid: user._id},"shhhh");
             res.cookie("token",token);
-            res.status(200).send("You can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login")
     })
@@ -73,13 +110,15 @@ app.get("/logout",(req,res)=>{
 }) 
 
 function isLoggedIn(req,res,next){
-    if(req.cookies.token ==="") res.send("you must be logged in");
+    if(req.cookies.token ==="") res.redirect("/login");
     else{
         let data = jwt.verify(req.cookies.token,"shhhh");
         req.user = data;
         next();
     }
 }
+
+
 
 
 app.listen(3000);
